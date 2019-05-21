8100fe99e370fab257ca151761cbaac53ad69c3e
吴超 <wuchaoj@enn.cn>
2019-05-21 18:15:05 +0800

    添加图片上传加载圈

--- src/components/uploadImg.vue
+++ src/components/uploadImg.vue
@@ -5,6 +5,8 @@
             <input id="profilePhotoFileUpload" v-if="upType" type="file" @change="getFile" accept="image/*" :capture="upType"/>
             <input v-else type="file" @change="getFile" accept="image/*"/>
         </div>
+        <div id="over" class="overLoading"></div>
+        <div id="layout" class="layoutLoading"><img src="../assets/loding.gif" /></div>
     </div>
 </template>
 <script>
@@ -31,10 +33,22 @@
                 this.$bmob.initialize('6cc51f95f96811441ba8e4754ac01f26', 'f3037695274e2983ee6b99b7e3e952b0')
                 let bmobFile = this.$bmob.File(file.name, file);
                 this.$emit('progress', 50)
+                this.showLoading(true)
                 bmobFile.save().then(res => {
+                    this.showLoading(false)
                     this.$emit('imageSrc', res[0])
                     this.$emit('progress', 100)
                 })
+            },
+            showLoading(show){
+                if(show){
+                    document.getElementById("over").style.display = "block";
+                    document.getElementById("layout").style.display = "block";
+                }else{
+                    document.getElementById("over").style.display = "none";
+                    document.getElementById("layout").style.display = "none";
+                }
+
             }
         },
         created () {
@@ -72,4 +86,23 @@
                 width 100%
                 height 100%
                 opacity: 0;
+        .overLoading
+            display: none;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #f5f5f5;
+            opacity:0.5;
+            z-index: 1000;
+        .layoutLoading
+            display: none;
+            position: absolute;
+            top: 40%;
+            left: 40%;
+            width: 20%;
+            height: 20%;
+            z-index: 1001;
+            text-align:center;
 </style>
